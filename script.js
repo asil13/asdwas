d47f94127a828eb879c85386cd10ac4ff0eb5a5a
asil13 <88874404+asil13@users.noreply.github.com>
2021-08-21 15:54:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+var a = +prompt('Введите число')
+
+switch (a) {
+    case 1:
+        alert ( '1 ночи' );
+        break;
+
+    case 2:
+        alert ( '2 ночи ' );
+        break;
+
+    case 3:
+        alert ( '3 ночи' );
+        break;
+
+    case 4:
+       alert ( '4 ночи' );
+       break;
+
+    case 5:
+        alert ( '5 ночи' );
+        break; 
+
+    case 6:
+        alert ( '6 утра' );
+        break;
+            
+    case 7:
+        alert ( '7 утра' );
+        break;
+            
+    case 8:
+        alert ( '8 утра' );
+        break;
+            
+    case 9:
+        alert ( '9 утра' );
+        break;
+            
+    case 10:
+        alert ( '10 утра' );
+        break;
+            
+    case 11:
+        alert ( '11 утра' );
+        break;
+            
+    case 12:
+        alert ( '12 утра' );
+        break;
+            
+    case 13:
+        alert ( '1 дня' );
+        break;
+            
+    case 14:
+        alert ( '2 часа дня' );
+        break;
+
+    case 15:
+        alert ( '3 часа дня' );
+        break;
+
+    case 16:
+        alert ( '4 часа дня' );
+        break;
+
+    case 17:
+        alert ( '5 часа дня' );
+        break;
+            
+    case 18:
+        alert ( '6 часа дня' );
+        break;
+        
+    case 19:
+        alert ( '7 вечера' );
+        break;
+
+    case 20:
+        alert ( '8 вечера' );
+        break;
+        
+    case 21:
+        alert ( '9 вечера' );
+        break;
+        
+    case 22:
+        alert ( '10 вечера' );
+        break;
+        
+    case 23:
+        alert ( '11 вечера' );
+        break;  
+        
+    case 24:
+        alert ( '12 вечера' );
+        break;    
+    default:
+        alert('Нету такого часа')
+}
